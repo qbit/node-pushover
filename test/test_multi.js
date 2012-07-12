ea62cdd09afedc93bbcd21ac1890fbb473be2e8e
Aaron Bieber <qbit@qbit.io>
2012-07-12 06:44:04 -0700

    added ability to pass token and user keys to .send method

---
+++ test/test_multi.js
@@ -0,0 +1,20 @@
+var push = require( '../lib/pushover.js' );
+
+var p = new push( {
+	// user: process.env['PUSHOVER_USER'],
+	token: process.env['PUSHOVER_TOKEN'],
+	debug: true
+});
+
+var msg = {
+	message: 'omg node test',
+	title: "Well - this is fantastic",
+  user: process.env['PUSHOVER_USER']
+};
+
+// console.log( p );
+
+p.send( msg, function( err, result ) {
+	console.log( err );
+	console.log( result );
+});
