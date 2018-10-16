95ecc766a9f742bb76a4ac5b14ce4bb3171618ea
Damien Clark <damo.clarky@gmail.com>
2018-10-16 21:02:01 +1000

    Implement attachment support using form-data module

---
+++ test/test_attachment.js
@@ -0,0 +1,25 @@
+var Push = require('../lib/pushover.js')
+var fs = require('fs')
+
+var p = new Push({
+  user: process.env['PUSHOVER_USER'],
+  token: process.env['PUSHOVER_TOKEN'],
+  update_sounds: false,
+  debug: true
+})
+
+var msg = {
+  message: 'test from ' + process.argv[1],
+  sound: 'magic',
+  title: 'Well - this is fantastic',
+  attachment: fs.createReadStream('../img/pushover-header.png')
+}
+
+// console.log( p );
+
+p.send(msg, function (err, result, res) {
+  console.log('error', err)
+  console.log('result', result)
+  console.log('res.headers', res.headers)
+  // process.exit(0);
+})
